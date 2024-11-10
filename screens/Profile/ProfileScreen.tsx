c85b9feac203d000b76c929dd37d6bc010dfc760
kirkram <94983351+kirkram@users.noreply.github.com>
2024-11-10 02:34:51 +0200

    fix the profile

--- screens/Profile/ProfileScreen.tsx
+++ screens/Profile/ProfileScreen.tsx
@@ -6,14 +6,14 @@ const HomeWelcome = ({ colorr }: { colorr: string }) => {
    const imageUrl3 = "https://static.wfolio.com/file/AqiFFw_TXMM4LDwoI2TPSYAM1lHVLAGB/F3eUE25SDaC3OUQ9TuvC-m2zOPPnCGNt/NhJnu4dzLtIECk2pLsZO4gmbavGKvbAt/GMEF1eteUjgUj_ciKSjdUShER99jTleL/oltXTSQ4krvlfCLio11ZmVg6B50KM80Y/FY2RqxZbo-o.png";
   
   return (
-    <ImageBackground source={{ uri: imageUrl3 }} style={[styles.header, { backgroundColor: colorr }]}>
-      <LinearGradient
+    <ImageBackground source={{ uri: imageUrl3 }} style={[styles.image3, { backgroundColor: colorr }]}>
+      {/* <LinearGradient
         colors={[Colors.ss.gradient1, Colors.ss.gradient2, 'transparent']}
         start={[0, 1]}
         end={[1, 0]}
-        style={styles.gradient}>
-            <Image source={{ uri: imageUrl3 }} style={styles.image3} />
-      </LinearGradient>
+        style={styles.gradient}> */}
+            {/* <Image source={{ uri: imageUrl3 }} style={styles.image3} /> */}
+      {/* </LinearGradient> */}
     </ImageBackground>
   );
 };
@@ -31,16 +31,11 @@ const styles = StyleSheet.create({
     marginTop: -20,
   },
   gradient: {
-    ...StyleSheet.absoluteFillObject,
+    // ...StyleSheet.absoluteFillObject,
     borderRadius: 10,
   },
   image3: {
-    // width: 100%,
-    // height: 100%,
-    // borderRadius: 25,
-    // position: 'absolute',
-    top: 600,
-    // right: 200,
+    flex: 1,
   },
 });
 
